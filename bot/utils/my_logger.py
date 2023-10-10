b0d3623f8bcb5f27b992b86abed04e866415899a
barabashka656 <alekseithebestofthebest@gmail.com>
2023-10-10 23:52:02 +0300

    update logger

--- bot/utils/my_logger.py
+++ bot/utils/my_logger.py
@@ -3,7 +3,7 @@ import logging
 from logging import handlers
 
 
-def configure_logger():
+def configure_root_logger():
     # Create a logger
     logging.basicConfig(level=logging.INFO)
     # Create a folder for log files
@@ -21,3 +21,25 @@ def configure_logger():
     handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
     # Add the handler to the root logger
     logging.getLogger().addHandler(handler)
+
+
+def setup_attempts_logger() -> logging.Logger:
+    # Create a logger
+    logging.basicConfig(level=logging.INFO)
+    # Create a folder for log files
+    log_folder = os.path.join('bot', 'data', 'logs')
+    if not os.path.exists(log_folder):
+        os.makedirs(log_folder)
+    # Create a rotating file handler
+    log_file = os.path.join(log_folder, 'attempts_logs.log')
+    handler = handlers.RotatingFileHandler(
+        filename=log_file,
+        maxBytes=1e7,
+        backupCount=5,
+        encoding='utf-8'
+    )
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    # Add the handler to the root logger
+    logger = logging.getLogger('attempts')
+    logger.addHandler(handler)
+    return logger
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 
-from bot.utils.my_logger import configure_logger
+from bot.utils.my_logger import configure_root_logger
 from bot.selenium_bot import (
     get_driver,
     config_browser,
@@ -10,10 +10,12 @@ from bot.selenium_bot import (
 logger = logging.getLogger(__name__)
 
 def main():
-    configure_logger()
+    configure_root_logger()
     driver = get_driver()
     config_browser(driver)
     start_play(driver)
 
+
 if __name__ == '__main__':
     main()
+    
